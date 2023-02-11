58918d3711c0d465d9e52dd211f8fcbeb504cace
Jan <jankopec7@gmail.com>
2023-02-11 10:35:48 +0100

    routing php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'ayoooo 👋';
+require 'Routing.php';
+
+$path = $_SERVER['REQUEST_URI'];
+$path = trim($path, '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('main_menu', 'DefaultController');
+Router::get('solo_game', 'DefaultController');
+
+Router::run($path);
